a203cbd61036e629c2a979cd8f78d65894c237d4
torreslucs23 <luquinhastorres231002@gmail.com>
2022-12-27 12:34:25 -0300

    added detailview and formview

--- reviews/views.py
+++ reviews/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
-class ReviewView(View):
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+'''
+    essa parte do código é para usar com View
     def get(self, request):
         form = ReviewForm()
 
@@ -26,6 +37,7 @@ class ReviewView(View):
         return render (request, "reviews/review.html", {
             "form": form
         })
+'''
 
 class thankYouView(TemplateView):
     template_name = "reviews/thank_you.html" #nome do caminho
@@ -48,16 +60,9 @@ class ReviewsListView(ListView):
 
 
 
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs['id']
-        selected_review = Review.objects.get(pk=review_id)
-        context['review'] = selected_review
-        return context
-
+    model = Review
 
 
 
