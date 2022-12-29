da28f68cd1b127c1fafbac1e51f3a9027035303d
torreslucs23 <luquinhastorres231002@gmail.com>
2022-12-29 14:57:28 -0300

    finished uploads session

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect    
-
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView 
 from .forms import ProfileForm
 from .models import UserProfile
 
 # Create your views here.
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model =  UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
 
+
+class ProfilesViews(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
+
+#essa classe foi utilizada usando a biblioteca View
+"""
 class CreateProfileView(View):
     def get(self, request):
         form = ProfileForm()
@@ -26,4 +40,4 @@ class CreateProfileView(View):
 
         return render(request, "profiles/create_profile.html",{
             "form": submiteted_form
-        })
+        })"""
